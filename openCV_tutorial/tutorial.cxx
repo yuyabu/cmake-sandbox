c802c0e983ff123d72b022f12f7f35aa7aa08139
yuyabu <yuyabushita@gmail.com>
2019-04-08 18:02:53 +0900

    add openCV site's first samples

---
+++ openCV_tutorial/tutorial.cxx
@@ -0,0 +1,17 @@
+// 1つの数値の平方根を計算する単純なプログラム
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main (int argc, char *argv[])
+{
+  if (argc < 2)
+    {
+    fprintf(stdout,"Usage: %s number\n",argv[0]);
+    return 1;
+    }
+  double inputValue = atof(argv[1]);
+  double outputValue = sqrt(inputValue);
+  fprintf(stdout,"The square root of %g is %g\n",
+          inputValue, outputValue);
+  return 0;
+}
